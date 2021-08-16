06bba6bb66f57d15e00fbf10193ff98532b3aa31
sayeed1999 <mdsayeedrahman1999@gmail.com>
2021-08-16 18:53:01 +0600

    feature04: save new teacher impelemented with all its dependent entities

---
+++ API Layer/Controllers/DesignationsController.cs
@@ -0,0 +1,33 @@
+﻿using Entity_Layer;
+using Microsoft.AspNetCore.Mvc;
+using Repository_Layer;
+using Service_Layer.DesignationService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Layer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DesignationsController : ControllerBase
+    {
+        private readonly IDesignationService _service;
+
+        public DesignationsController(IDesignationService service)
+        {
+            this._service = service;
+        }
+
+        // GET: Designations
+        [HttpGet]
+        public async Task<ActionResult<ServiceResponse<IEnumerable<Designation>>>> GetDesignations()
+        {
+            var serviceResponse = await _service.GetAll();
+            if (serviceResponse.Success == false) return BadRequest(serviceResponse);
+            return Ok(serviceResponse);
+        }
+
+    }
+}
