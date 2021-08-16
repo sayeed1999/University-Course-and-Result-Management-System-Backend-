21730e03da4be27b03ff0d17e1ea073c56defddb
sayeed1999 <mdsayeedrahman1999@gmail.com>
2021-08-16 14:52:44 +0600

    added Departments to Entity Layer & Data Layer

---
+++ Data Access Layer/ApplicationDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Entity_Layer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=sayeeds-coding-\sqlexpress;Database=UniversityCourseAndResultManagementSystem;trusted_connection=SSPI");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            /// Table: Departments
+
+            builder.Entity<Department>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+                entity.Property(x => x.Code).IsRequired().HasMaxLength(7);
+                entity.HasIndex(x => x.Code).IsUnique();
+                entity.HasCheckConstraint("CHK_LengthOfCode", "len(code) >= 2 and len(code) <= 7");
+                entity.Property(x => x.Name).IsRequired().HasMaxLength(255);
+                entity.HasIndex(x => x.Name).IsUnique();
+                entity.HasCheckConstraint("CHK_LengthOfDeptName", "len(name) >= 3");
+                entity.HasData(
+                    new Department { Id = 1, Code = "EEE", Name = "Electronics & Electrical Engineering" },
+                    new Department { Id = 2, Code = "CSE", Name = "Computer Science & Engineering" },
+                    new Department { Id = 3, Code = "CE", Name = "Civil Engineering" },
+                    new Department { Id = 4, Code = "ME", Name = "Mechanical Engineering" },
+                    new Department { Id = 5, Code = "MTE", Name = "Mechatronics Engineering" },
+                    new Department { Id = 6, Code = "IPE", Name = "Industrial Production & Engineering" }
+                );
+            });
+
+        }
+    }
+}
---
+++ Data Access Layer/Migrations/20210816085140_AddDepartmentsTable.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data_Access_Layer.Migrations
+{
+    public partial class AddDepartmentsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(type: "nvarchar(7)", maxLength: 7, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.Id);
+                    table.CheckConstraint("CHK_LengthOfCode", "len(code) >= 2 and len(code) <= 7");
+                    table.CheckConstraint("CHK_LengthOfDeptName", "len(name) >= 3");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Departments",
+                columns: new[] { "Id", "Code", "Name" },
+                values: new object[,]
+                {
+                    { 1, "EEE", "Electronics & Electrical Engineering" },
+                    { 2, "CSE", "Computer Science & Engineering" },
+                    { 3, "CE", "Civil Engineering" },
+                    { 4, "ME", "Mechanical Engineering" },
+                    { 5, "MTE", "Mechatronics Engineering" },
+                    { 6, "IPE", "Industrial Production & Engineering" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departments_Code",
+                table: "Departments",
+                column: "Code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departments_Name",
+                table: "Departments",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Departments");
+        }
+    }
+}
