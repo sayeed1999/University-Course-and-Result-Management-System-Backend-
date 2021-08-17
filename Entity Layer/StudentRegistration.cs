6a769f5ec7bf835470018c7c6e506b8cb6835a2a
sayeed1999 <mdsayeedrahman1999@gmail.com>
2021-08-17 12:13:30 +0600

    Added Students Entity for the DB and StudentRegistration entity for the registration form

---
+++ Entity Layer/StudentRegistration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Layer
+{
+    public class StudentRegistration
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Contact { get; set; }
+        public DateTime Date { get; set; }
+        public string Address { get; set; }
+        public int DepartmentId { get; set; }
+        public Department? Department { get; set; }
+    }
+}
