f180163949e2bc21c8c2cf8df44770ab90b6ffb5
sayeed1999 <mdsayeedrahman1999@gmail.com>
2021-08-17 11:54:29 +0600

    feature06: get courses by department impelented

--- Service Layer/CourseService/ICourseService.cs
+++ Service Layer/CourseService/ICourseService.cs
@@ -11,5 +11,6 @@ namespace Service_Layer.CourseService
     public interface ICourseService : IRepository<Course>
     {
         public Task<ServiceResponse<Course>> GetByCompositeKey(int departmentId, string courseCode);
+        public Task<ServiceResponse<IEnumerable<Course>>> GetCoursesByDepartment(int departmentId);
     }
 }
