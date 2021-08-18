0b7bf1248ebe208ba5261d161ea3c2f3639f4877
sayeed1999 <mdsayeedrahman1999@gmail.com>
2021-08-18 22:09:17 +0600

    gradeletter entity created & refered to studentcourse

--- Entity Layer/StudentsCourses.cs
+++ Entity Layer/StudentsCourses.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,9 @@ namespace Entity_Layer
         public string CourseCode { get; set; }
         public long StudentId { get; set; }
         public DateTime Date {  get; set; }
+        public string Grade {  get; set; }
+        [ForeignKey("Grade")]
+        public GradeLetter? GradeLetter { get; set; }
         public Student? Student {  get; set; }
         public Course? Course {  get; set; }
     }
