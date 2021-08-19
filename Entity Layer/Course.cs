35f75ec1ae6bcfabe988d7dd0b3bef691f15e582
sayeed1999 <mdsayeedrahman1999@gmail.com>
2021-08-19 18:00:31 +0600

    bug fixed in Course entity, initialized the ICollection objects

--- Entity Layer/Course.cs
+++ Entity Layer/Course.cs
@@ -8,6 +8,11 @@ namespace Entity_Layer
 {
     public class Course
     {
+        public Course()
+        {
+            this.AllocateClassrooms = new HashSet<AllocateClassroom>();
+            this.StudentsCourses = new HashSet<StudentCourse>();
+        }
         // Code + DepartmentId combinely forms the Composite Key which is the PK for thist table. This table has no single row PK.
         public string Code { get; set; }
         public string Name { get; set; }
