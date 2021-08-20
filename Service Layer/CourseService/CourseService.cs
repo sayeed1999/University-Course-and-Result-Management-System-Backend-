e5c48eac891ef910ead15137378b531fdfd4a3d6
sayeed1999 <mdsayeedrahman1999@gmail.com>
2021-08-20 16:00:05 +0600

    feature03: save course modified

--- Service Layer/CourseService/CourseService.cs
+++ Service Layer/CourseService/CourseService.cs
@@ -1,5 +1,6 @@
 ﻿using Data_Access_Layer;
 using Entity_Layer;
+using Microsoft.EntityFrameworkCore;
 using Repository_Layer;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,34 @@ namespace Service_Layer.CourseService
     public class CourseService : Repository<Course>, ICourseService
     {
         public CourseService(ApplicationDbContext dbContext) : base(dbContext) { }
+
+        public override async Task<ServiceResponse<Course>> Add(Course item)
+        {
+            var serviceResponse = new ServiceResponse<Course>();
+
+            // find if there remains a course with the same name in the same department
+            if (await _dbContext.Courses.SingleOrDefaultAsync(x => (x.Code == item.Code || x.Name == item.Name) && x.DepartmentId == item.DepartmentId) == null)
+            {
+                serviceResponse.Data = item;
+                serviceResponse.Message = "Code and name must be unique in the respective department";
+                serviceResponse.Success = false;
+                return serviceResponse;
+            }
+
+            try
+            {
+                _dbContext.Courses.Add(item);
+                await _dbContext.SaveChangesAsync();
+                serviceResponse.Data = item;
+                serviceResponse.Message = "Item stored successfully to the database.";
+            }
+            catch (Exception ex)
+            {
+                serviceResponse.Message = ex.Message;
+                serviceResponse.Success = false;
+            }
+            return serviceResponse;
+        }
+
     }
 }
