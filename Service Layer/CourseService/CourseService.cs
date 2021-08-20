a494f175bdf042fec19e5892e822829a671e8c93
sayeed1999 <mdsayeedrahman1999@gmail.com>
2021-08-20 23:48:11 +0600

    more modifications

--- Service Layer/CourseService/CourseService.cs
+++ Service Layer/CourseService/CourseService.cs
@@ -65,6 +65,26 @@ namespace Service_Layer.CourseService
             }
             return serviceResponse;
         }
+        public async Task<ServiceResponse<IEnumerable<Course>>> GetCoursesByDepartmentIncludingTeachersAndSemisters(int departmentId)
+        {
+            var serviceResponse = new ServiceResponse<IEnumerable<Course>>();
+            try
+            {
+                serviceResponse.Data = await _dbContext.Courses
+                    .Include(x => x.Teacher)
+                    .Include(x => x.Semister)
+                    .Where(x => x.DepartmentId == departmentId)
+                    .ToListAsync();
+
+                serviceResponse.Message = "Data fetched successfully from the database";
+            }
+            catch (Exception ex)
+            {
+                serviceResponse.Message = "Some error occurred while fetching data.\nError message: " + ex.Message;
+                serviceResponse.Success = false;
+            }
+            return serviceResponse;
+        }
 
         public async Task<ServiceResponse<IEnumerable<Course>>> GetCoursesByDepartmentIncludingTeachers(int departmentId)
         {
