11c075a4b49f6dc418917d9f2e122a01f5ec5405
sayeed1999 <mdsayeedrahman1999@gmail.com>
2021-08-24 11:55:22 +0600

    two separate studentsresults controller added for separate student result fetching

--- Service Layer/StudentService/IStudentService.cs
+++ Service Layer/StudentService/IStudentService.cs
@@ -11,6 +11,8 @@ namespace Service_Layer.StudentService
     public interface IStudentService : IRepository<Student>
     {
         public Task<ServiceResponse<IEnumerable<Student>>> GetStudentsResults();
+        public Task<ServiceResponse<Student>> GetStudentResultById(long id);
+        public Task<ServiceResponse<Student>> GetStudentResultByRegNo(String reg);
         public Task<ServiceResponse<Student>> RegisterStudent(StudentRegistration student);
         public Task<ServiceResponse<StudentCourse>> EnrollStudentInCourse(StudentCourse data);
         public Task<ServiceResponse<StudentCourse>> SaveResult(StudentCourse data);
