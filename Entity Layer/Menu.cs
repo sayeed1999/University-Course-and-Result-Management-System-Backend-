4161c48b3b2c46bbc6b9efa300c323a6efaf9fc2
sayeed1999 <mdsayeedrahman1999@gmail.com>
2021-08-26 14:44:05 +0600

    menu entity created and added to database

---
+++ Entity Layer/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Layer
+{
+    public class Menu
+    {
+        public int Id {  get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Url {  get; set; }
+        [Required]
+        public Status StatusId { get; set; } = Status.Inactive;
+        public Menu? Parent {  get; set; }
+        public int? ParentId { get; set; } = null;
+    }
+
+    public enum Status
+    {
+        Active, // 0
+        Inactive, // 1
+    }
+}
